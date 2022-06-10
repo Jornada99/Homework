a47181bc62f9934f0feec76bcbe5e21265dcf0d4
Alex <93819279+Jornada99@users.noreply.github.com>
2022-06-10 22:56:20 +0300

    Update 5l_АИ.swift

--- 5l_АИ.swift
+++ 5l_АИ.swift
@@ -1,10 +1,12 @@
+import Foundation
+
+
 protocol Car {
     var carModel: String {get set}
     var year: Int {get set}
     var trunkVolume: Int {get set}
     
     func putInTrunk()
-    func windows()
     func engine()
 }
 
@@ -13,18 +15,12 @@ extension Car {
 }
 
 extension Car {
-    func windowsOpen() {}
-}
-
-extension Car {
     func engine() {}
 }
 
 
 // first class
-
 class TrunkCar: Car {
-
     var carModel: String
     var year: Int
     var trunkVolume: Int
@@ -33,7 +29,7 @@ class TrunkCar: Car {
     let loadCapacity: LoadCapacityEnum
     let numberOfPallets: NumberOfPalletsEnum
     
-     enum LoadCapacityEnum {
+    enum LoadCapacityEnum: Int {
         case oneT = 1
         case twoT = 2
         case threeT = 3
@@ -41,7 +37,7 @@ class TrunkCar: Car {
         case sevenT = 7
     }
     
-    enum NumberOfPalletsEnum {
+    enum NumberOfPalletsEnum: Int {
         case fourP = 4
         case sixP = 6
         case eightP = 8
@@ -75,58 +71,56 @@ class TrunkCar: Car {
         
         switch engineMode {
             case .engineIsOn:
-                print("The engine of trunk car is switched on.")
+                print("\(self.carModel) truck engine on.")
             case .engineIsOff:
-                print("The engine of trunk car is switched off.")
+                print("\(self.carModel) truck engine off.")
         }
         
     }
     
     func windows(windowsMode: WindowsMode) {
-    
+        
         switch windowsMode {
             case .windowsOpen:
-                print("Open the windows of trunk car.")
+                print("\(self.carModel) truck windows open.")
             case .windowsClose: 
-                print("Close the windows of trunk car.")
+                print("\(self.carModel) truck windows closed.")
         }
         
-    init(carModel: String, year: Int, trunkVolume: Int, loadCapacity: LoadCapacityEnum,
-    numberOfPallets: NumberOfPalletsEnum) {
+    }
+        
+    init(carModel: String, year: Int, trunkVolume: Int, loadCapacity: LoadCapacityEnum, numberOfPallets: NumberOfPalletsEnum) {
         self.carModel = carModel
         self.year = year
         self.trunkVolume = trunkVolume
-        self.loadCapacity = LoadCapacityEnum
-        self.numberOfPallets = NumberOfPalletsEnum
+        self.loadCapacity = loadCapacity
+        self.numberOfPallets = numberOfPallets
     }
+}
         
-    }
-    
-    
-    // second class
-    
-    class SportCar: Car {
-    
+        
+// second class
+class SportCar: Car {
     var carModel: String
     var year: Int
     var trunkVolume: Int
-    
+        
     // special properties
     let maxSpeed: Int
     let type: String
-    
+        
     enum EngineMode {
         case engineIsOn 
         case engineIsOff
     }
-    
+        
     enum WindowsMode {
         case windowsOpen
         case windowsClose
     }
         
     func putInTrunk(goodsVolume: Int) -> String {
-        
+            
         if goodsVolume < self.trunkVolume {
             let difference = self.trunkVolume - goodsVolume
             return "Available \(difference) units of volume."
@@ -136,43 +130,44 @@ class TrunkCar: Car {
         } else {
             return "Trunk is fulled."
         }
-        
+            
     }    
         
     func engine(engineMode: EngineMode) {
-        
+            
         switch engineMode {
             case .engineIsOn:
-                print("The engine of sport car is switched on.")
+                print("\(self.carModel) sports car engine on.")
             case .engineIsOff:
-                print("The engine of sport car is switched off.")
-        }
-        
-    }
-    
-    func windows(windowsMode: WindowsMode) {
-    
-        switch windowsMode {
-            case .windowsOpen:
-                print("Open the windows of sport car.")
-            case .windowsClose: 
-                print("Close the windows of sport car.")
+                print("\(self.carModel) sports car engine off.")
         }
             
-    func speed(speed: Int) { // new special action
-    
+    }
+        
+    func windows(windowsMode: WindowsMode) {
+            
+        switch windowsMode {
+            case .windowsOpen:
+                print("\(self.carModel) sports car windows open.")
+            case .windowsClose: 
+                print("\(self.carModel) sports car windows closed.")
+        }
+            
+    }
+        
+    func speed(speed: Int) { 
+            
         if speed == self.maxSpeed {
             print("You have reached the maximum speed")
         } else if speed > self.maxSpeed {
-            self.engineOn = false
             print("Emergency stop.")
         } else {
             let difference = self.maxSpeed - speed
             print("\(difference) km / h left before reaching the maximum speed.")
         }
-        
+            
     }
-    
+        
     init(carModel: String, year: Int, trunkVolume: Int, maxSpeed: Int, type: String) {
         self.carModel = carModel
         self.year = year
@@ -180,18 +175,16 @@ class TrunkCar: Car {
         self.maxSpeed = maxSpeed
         self.type = type
     }
-    
 }
-
-    extension TrunkCar: CustomStringConvertible {
-        var description: String {
-            return "Car: \(carModel), year: \(year), trunk volume: \(trunkVolume), load capacity: \(loadCapacity), number of pallets: \(numberOfPallets)."
-        }
+        
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "Car: \(carModel), year: \(year), trunk volume: \(trunkVolume), load capacity: \(loadCapacity), number of pallets: \(numberOfPallets)."
     }
-    
-    extension SportCar: CustomStringConvertible {
-        var description: String {
-            return "Car: \(carModel), year: \(year), trunk volume: \(trunkVolume), max speed: \(maxSpeed), type: \(type)."
-        }
+}
+        
+extension SportCar: CustomStringConvertible {
+    var description: String {
+        return "Car: \(carModel), year: \(year), trunk volume: \(trunkVolume), max speed: \(maxSpeed), type: \(type)."
     }
-    
+}
